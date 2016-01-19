bbf91c909958154b9e92d45e6327838f91a44bc6
zkphill <zkphill@mail.com>
2016-01-18 21:27:20 -0500

    put collatz functions into one collatz function
    removed function collatz_even and collatz_odd
    put into collatz_calculator

--- main.py
+++ main.py
@@ -6,36 +6,28 @@ Created on Jan 18, 2016
 
 # collatz conjuecture calculator
 
-
+# output file
 fout = open("output.txt", "w")
 
 # bounds for range of input numbers
 lower_bound = 1
 upper_bound = 101
 
-# processes even numbers
-def collatz_even(num):
-    return num // 2
-
-# processes odd numbers
-def collatz_odd(num):
-    return num * 3 + 1
 
 # takes input and sends to odd or even function
-def collatz_main(num):
+def collatz_calculator(num):
     if num % 2 == 0:
-        return collatz_even(num)
+        return num // 2
     else:
-        return collatz_odd(num)
+        return num * 3 + 1
 
 
-# tests main function
-
+# tests collatz calculator
 def collatz_test(lower_bound, upper_bound):
     for num in range(lower_bound, upper_bound):
         fout.write(str(num) + " ")
         while num > 1:
-            num = collatz_main(num)
+            num = collatz_calculator(num)
             fout.write(str(num) + " ")
         fout.write("\n" + "\n")
     

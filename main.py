2683ed6c574e3e62c7a929b770adbac0655fc517
zkphill <zkphill@mail.com>
2016-01-19 21:21:18 -0500

    calculates run time for a range of input numbers

--- main.py
+++ main.py
@@ -3,11 +3,10 @@ Created on Jan 18, 2016
 
 @author: zkphi
 '''
+import timeit
 
 # collatz conjuecture calculator
 
-# output file
-fout = open("output.txt", "w")
 
 # implements the collatz conjucture algorithm
 def collatz_calculator(num):
@@ -17,17 +16,17 @@ def collatz_calculator(num):
         return num * 3 + 1
 
 # tests collatz calculator
-def collatz_test(lower_bound, upper_bound):
+def collatz_test(upper_bound, lower_bound):
     for num in range(lower_bound, upper_bound):
-        fout.write(str(num) + " ")
         while num > 1:
-            num = collatz_calculator(num)
-            fout.write(str(num) + " ")
-        fout.write("\n" + "\n")
+            num = collatz_calculator(num)    
 
 lower_bound = input("enter the first number : ")
 upper_bound = input("enter last number : ") + 1
-  
-collatz_test(lower_bound, upper_bound)
-        
-fout.close()
+
+print "start"
+
+# calculates runtime  
+t = timeit.Timer(lambda: collatz_test(upper_bound, lower_bound))
+
+print t.timeit(number = 1)
